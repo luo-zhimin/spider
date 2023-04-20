b92e23022f72b3491bb90aa8b3806bf506970e44
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-04-20 22:51:43 +0800

    fix cookie

--- study_urllib/urllib_cookie.py
+++ study_urllib/urllib_cookie.py
@@ -1,17 +1,31 @@
 # 微博Cookie登陆 数据采集得时候 绕过登陆 进入采集
 
 import urllib.request
-import urllib.parse
 import urllib.error
 
-url = 'https://m.weibo.cn/profile/info'
+# 替换成您自己的Cookie
+cookie = 'SUB=_2A25JOssBDeRhGeNG4lcQ9yzLwjqIHXVqxNVJrDV6PUJbkdANLUGnkW1NSuKXQn7TgkzBofpYhHuMoAZ8XrC3QZSy; ' \
+         '_T_WM=57799680770; MLOGIN=1; WEIBOCN_FROM=1110106030; M_WEIBOCN_PARAMS=luicode=20000174&uicode=20000174; ' \
+         'XSRF-TOKEN=9dd710; mweibo_short_token=4487276b72'
 
+# 替换成您自己的用户ID
+user_id = '5895172796'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/112.0.0.0 Safari/537.36',
-    # "Accept": '*/*',
-    'cookie': 'WEIBOCN_FROM=1110006030; SUB=_2A25JOssBDeRhGeNG4lcQ9yzLwjqIHXVqxNVJrDV6PUJbkdANLUGnkW1NSuKXQn7TgkzBofpYhHuMoAZ8XrC3QZSy; _T_WM=57799680770; MLOGIN=1; XSRF-TOKEN=d582e8; mweibo_short_token=a65fb62c77; M_WEIBOCN_PARAMS=luicode=20000174&uicode=20000174'
+    'referer': 'https://m.weibo.cn/profile/5895172796',
+    'X-XSRF-TOKEN': '9dd710',
+    'cookie': cookie,
+    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': 'macOS',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same - origin'
 }
+
+# 构造请求URL
+url = f'https://m.weibo.cn/profile/info?uid={user_id}'
 
 request = urllib.request.Request(url=url, headers=headers)
 
