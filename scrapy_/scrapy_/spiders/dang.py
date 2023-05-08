aa9eec195ab7d656198acfa87fd70d786e5a21ea
镜像 <68786940+luo-zhimin@users.noreply.github.com>
2023-05-08 22:56:57 +0800

    many page download

--- scrapy_/scrapy_/spiders/dang.py
+++ scrapy_/scrapy_/spiders/dang.py
@@ -6,8 +6,12 @@ from scrapy_.items import ScrapyItem
 
 class DangSpider(scrapy.Spider):
     name = "dang"
+    # 如果是多页下载的话 需要调整 allowed_domains 一般情况只写 域名
     allowed_domains = ["category.dangdang.com"]
     start_urls = ["http://category.dangdang.com/cp01.31.04.00.00.00.html"]
+
+    base_url = 'http://category.dangdang.com/pg'
+    page = 1
 
     def parse(self, response):
         print("====dang====")
@@ -38,3 +42,13 @@ class DangSpider(scrapy.Spider):
             # 获取一个book就将book交给pipelines
             yield book
         # pass
+
+        # 每一页爬取的业务逻辑一样 只需要改页码 并且再次执行parse方法
+        # pg2-....pg100
+        if self.page < 100:
+            self.page = self.page + 1
+            url = self.base_url + str(self.page) + "cp01.31.04.00.00.00.html"
+            # 怎么去调用parse方法
+            # scrapy.Request就是scrapy的get请求
+            # url 就是请求地址 callback 就是请求的函数 不需要加()
+            yield scrapy.Request(url=url, callback=self.parse)
